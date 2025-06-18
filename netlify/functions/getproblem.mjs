e969e49a867248db2aa53733e43c788db25d60a2
sclass53 <gino0922@163.com>
2025-06-18 22:16:47 +0800

    x

--- netlify/functions/getproblem.mjs
+++ netlify/functions/getproblem.mjs
@@ -2,5 +2,5 @@ export default async (req, context) => {
   console.log(req.body);
   console.log(req);
   console.log(typeof(req.body))
-  return new Response(req.body);
+  return new Response(typeof(req.body));
 };
